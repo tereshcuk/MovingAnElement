fba0802bcb17172e00cd03cec9d4cca17e4f923d
Nickolay <tereshcuk@yandex.ru>
2026-01-18 14:45:35 +0300

    ДЗ устранения замечаний

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./css/style.css";
-
 import "./js/app";
 
+
 // TODO: write your code in app.js
+
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,49 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cells = document.querySelectorAll(".cell");
-  const img = document.createElement("div");
-  img.className = "goblin";
-  let currentCell = null;
+import goblin_png from "../img/goblin.png";
 
+document.addEventListener("DOMContentLoaded", () => {
+
+  const gameСontainer = document.createElement("div");
+  const body =  document.body;
+  gameСontainer.classList.add("game-container");
+
+  for (let i = 0; i < 16; i++) {
+    const newDiv = document.createElement('div');    
+    newDiv.className = 'cell';    
+    gameСontainer.append(newDiv);
+    }
+  
+  body.append(gameСontainer);
+
+  const cells = document.querySelectorAll(".cell");
+  
+  const img = document.createElement('img');
+  img.src = goblin_png; 
+  img.alt = "Это гоблин!!!";    
+
+  let currentCell = null;
+  
   function getCell() {
     const index = Math.floor(Math.random() * cells.length);
     return cells[index];
   }
 
   function movieGoblin() {
-    const newCell = getCell();
+
+    if (currentCell) {
+      let _img = currentCell.querySelector("img");
+      if (_img){
+        //console.log("Есть рисунок в текущей ячейке!!!");
+        currentCell.removeChild(img);
+      }      
+    }
+    const newCell = getCell();             
     if (newCell !== currentCell) {
-      currentCell = newCell;
-      currentCell.appendChild(img);
+      currentCell = newCell;      
+      currentCell.append(img);      
     }
   }
 
-  setInterval(movieGoblin, 1000);
+  let timerId = setInterval(movieGoblin, 1000);
+  setTimeout(() => { clearInterval(timerId); alert("Игра окончена!"); }, 15000);
+
 });
